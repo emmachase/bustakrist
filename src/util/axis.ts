e264c6edf0362a318d9fb88582bf5ab1f8b5898c
root <root@DESKTOP-83M2SKC.localdomain>
2021-02-15 17:03:11 -0600

    I did my best

---
+++ src/util/axis.ts
@@ -0,0 +1,13 @@
+/**
+ * Returns the major interval (rate at which axis ticks are drawn) for a given axis
+ *
+ * @param axisMin - The lowest point drawn on this axis (0 for time, 1 for multiplier)
+ * @param axisMax - The highest point drawn on this axis (the greater of 10 or time, or the greater of 2 or multiplier)
+ */
+export function getMajorInterval(axisMin: number, axisMax: number): number {
+  const range = axisMax - axisMin;
+  let interval = 0.2;
+  for (let idx = 0; interval < range / 6; idx ++)
+    interval *= 2.5 - idx % 2 * 0.5;
+  return interval;
+}
